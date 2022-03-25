a6faa316420d838156e9bc6229bf1b9d9e56969b
Lin Lan <lanlin@meideng.net>
2022-03-25 14:30:04 +0800

    结构体、字符串、静态变量

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <cstring>
+#include <string>
+
+struct Student {
+    std::string name;
+    char desc[50];
+};
+
+int add(int a) {
+    static int sum = 0;
+    return sum += a;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!"
+              << add(1)
+              << add(3)
+              << std::endl;
+
+    auto s = Student{};
+    s.name = "lewin";
+    strcpy(s.desc, "这是一个普通的学生");
+
+    std::cout << s.name;
+
     return 0;
 }
