61dc15a5da0e09c5d7e247fb8735e7adfba33371
Lin Lan <lanlin@meideng.net>
2022-03-27 21:36:54 +0800

    重载

--- main.cpp
+++ main.cpp
@@ -15,6 +15,9 @@ public:
     Rectangle(int w, int h) : width(w), height(h) {
         cout << "正在创建Rectangle " << this->width << ' ' << this->height << endl;
     }
+    Rectangle(int w) : width(w), height(w) {
+        cout << "正在创建Rectangle " << this->width << ' ' << this->height << endl;
+    }
 
     ~Rectangle() {
         cout << "正在析构Rectangle " << this->width << ' ' << this->height << endl;
@@ -22,6 +25,11 @@ public:
 
     virtual int Area() {
         return this->width * this->height;
+    }
+
+    Rectangle operator+(Rectangle &b){
+        Rectangle r(this->width + b.width, this->height + b.height);
+        return r;
     }
 };
 
@@ -38,11 +46,18 @@ public:
     int Area() override {
         return this->width * this->width;
     }
+
+    Square operator+(Square &b){
+        Square r(this->width + b.width);
+        return r;
+    }
 };
 
 int main() {
-    Square r(2);
-    cout << r.Area() << endl;
+    Square r1(2);
+    Square r2(3);
+    auto r3 = r1 + r2;
+    cout << r3.Area() << endl;
 
     return 0;
 }
