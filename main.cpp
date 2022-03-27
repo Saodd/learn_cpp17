ffd0d32e67900c4bf90883917a90d333bbbb610f
Lin Lan <lanlin@meideng.net>
2022-03-27 12:21:43 +0800

    数组基本用法

--- main.cpp
+++ main.cpp
@@ -2,17 +2,18 @@
 #include <cstring>
 #include <string>
 
-struct Student {
-    std::string name;
-    char desc[50];
-};
 
 int add(int a) {
     static int sum = 0;
     return sum += a;
 }
 
-int main() {
+void playStruct() {
+    struct Student {
+        std::string name;
+        char desc[50];
+    };
+
     std::cout << "Hello, World!"
               << add(1)
               << add(3)
@@ -23,6 +24,22 @@ int main() {
     strcpy(s.desc, "这是一个普通的学生");
 
     std::cout << s.name;
+};
+
+/**
+ * 数组变量实际上是第1项的内存位置（指针）。
+ * 直接做指针计算，+1加的不是1字节，而是数组元素的尺寸（例如double是8）。
+ */
+void playArray() {
+    double numbers[10] = {};
+    for (int i = 0; i < 10; i++) {
+        numbers[i] = i + 100;
+    }
+    std::cout << numbers << std::endl << numbers + 1;
+}
+
+int main() {
+    playArray();
 
     return 0;
 }
