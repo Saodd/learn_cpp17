8538b2dca024be943a1dff0fb03bbdcadd74222c
Lin Lan <lanlin@meideng.net>
2022-03-27 20:18:15 +0800

    时间与读写文件

--- main.cpp
+++ main.cpp
@@ -1,36 +1,25 @@
 #include <iostream>
 #include <cstring>
 #include <string>
+#include <ctime>
+#include <fstream>
 
 using namespace std;
 
-double vals[] = {1, 2,3,4,5};
-
-double *setValues1(int i) {
-    double *ref = &vals[i];
-    return ref;
-}
-
-
-double &setValues(int i) {
-    double &ref = vals[i];
-    return ref;
-}
 
 int main() {
-    cout << "改变前的值" << endl;
-    for (int i = 0; i < 5; i++) {
-        cout << "vals[" << i << "] = ";
-        cout << vals[i] << endl;
-    }
+    auto now = time(nullptr);
+    cout << "本地日期和时间：" << ctime(&now) << endl;
 
-    setValues(1) = 666; // 引用
-    *setValues1(3) = 2333;  // 指针
-
-    cout << "改变后的值" << endl;
-    for (int i = 0; i < 5; i++) {
-        cout << "vals[" << i << "] = ";
-        cout << vals[i] << endl;
+    string line;
+    fstream f;
+    // ios::in是读取文件，ios::out是写入文件
+    f.open(R"(C:\Users\LanLin\code\learn\learn-cpp17\main.cpp)", ios::in);
+    if (f.is_open()) {
+        while (getline(f, line)) {
+            cout << line << '\n';
+        }
+        f.close();
     }
     return 0;
 }
