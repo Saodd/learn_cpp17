67541c1dd0f92ae0dff50928011efe57beb73b10
Lin Lan <lanlin@meideng.net>
2022-03-27 21:40:18 +0800

    多态

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ public:
     Rectangle(int w, int h) : width(w), height(h) {
         cout << "正在创建Rectangle " << this->width << ' ' << this->height << endl;
     }
+
     Rectangle(int w) : width(w), height(w) {
         cout << "正在创建Rectangle " << this->width << ' ' << this->height << endl;
     }
@@ -24,10 +25,11 @@ public:
     }
 
     virtual int Area() {
+        cout << "Rectangle Area" << endl;
         return this->width * this->height;
     }
 
-    Rectangle operator+(Rectangle &b){
+    Rectangle operator+(Rectangle &b) {
         Rectangle r(this->width + b.width, this->height + b.height);
         return r;
     }
@@ -44,20 +46,27 @@ public:
     }
 
     int Area() override {
+        cout << "Square Area" << endl;
         return this->width * this->width;
     }
 
-    Square operator+(Square &b){
+    Square operator+(Square &b) {
         Square r(this->width + b.width);
         return r;
     }
 };
 
 int main() {
-    Square r1(2);
-    Square r2(3);
-    auto r3 = r1 + r2;
-    cout << r3.Area() << endl;
+    Rectangle *ptr;
+
+    Rectangle r(2);
+    Square s(1);
+
+    ptr = &s;
+    ptr->Area();
+
+    ptr = &r;
+    ptr->Area();
 
     return 0;
 }
