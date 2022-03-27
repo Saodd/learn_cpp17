4b76e34a2cdab1558ef991b3a902903d74026df1
Lin Lan <lanlin@meideng.net>
2022-03-27 20:27:46 +0800

    结构体操作

--- main.cpp
+++ main.cpp
@@ -6,20 +6,38 @@
 
 using namespace std;
 
+struct Books {
+    char title[50];
+    char author[50];
+    string subject;
+    int book_id;
+};
+
+// 该函数以结构指针作为参数
+void printBook(Books *book) {
+    cout << "书标题  : " << book->title << endl;
+    cout << "书作者 : " << book->author << endl;
+    cout << "书类目 : " << book->subject << endl;
+    cout << "书 ID : " << book->book_id << endl;
+}
+
 
 int main() {
-    auto now = time(nullptr);
-    cout << "本地日期和时间：" << ctime(&now) << endl;
+    Books Book1{
+            .title =  "123",
+            .author = "lewin",
+            .subject = "一些内容",
+            .book_id = 1,
+    };        // 定义结构体类型 Books 的变量 Book1
+    Books Book2{};        // 定义结构体类型 Books 的变量 Book2
 
-    string line;
-    fstream f;
-    // ios::in是读取文件，ios::out是写入文件
-    f.open(R"(C:\Users\LanLin\code\learn\learn-cpp17\main.cpp)", ios::in);
-    if (f.is_open()) {
-        while (getline(f, line)) {
-            cout << line << '\n';
-        }
-        f.close();
-    }
+    strcpy(Book2.title, "我是标题");
+    strcpy(Book2.author, "我是作者");
+    Book2.subject = "另一些内容";
+    Book2.book_id = 12346;
+
+    printBook(&Book1);
+    printBook(&Book2);
+
     return 0;
 }
